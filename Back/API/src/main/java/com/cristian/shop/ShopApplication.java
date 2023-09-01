f524690ac64e4e6a00f7249fcc1cb5c485f7b535
CristianRodMar <alumno.138249@ies-azarquiel.es>
2023-09-01 19:50:10 +0200

    Generado nuevo jar

--- Back/API/src/main/java/com/cristian/shop/ShopApplication.java
+++ Back/API/src/main/java/com/cristian/shop/ShopApplication.java
@@ -1,5 +1,7 @@
 package com.cristian.shop;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShopApplication.class, args);
+		SpringApplication app = new SpringApplication(ShopApplication.class);
+        String port = System.getenv("PORT");
+        app.setDefaultProperties(Collections.singletonMap("server.port", port == null ? "8080" : port));
+        app.run(args);
 	}
+
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(ShopApplication.class, args);
+	// }
 
 }
